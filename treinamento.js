0e38361df991e1bd8bc034dadd576b885df421db
Victor <victorantonio.vats1@gmail.com>
2024-10-25 11:26:02 -0300

    responsividade_img

--- treinamento.js
+++ treinamento.js
@@ -1,11 +1,14 @@
 const pilotos = document.getElementById('pilotos');
 const construtores = document.getElementById('construtores');
+const im = document.getElementById('im');
 
 function mostrarPilotos() {
     pilotos.style.display = 'block';
     construtores.style.display = 'none';
+    im.style.height = '1310px'
 }
 function mostrarConstrutores() {
     pilotos.style.display = 'none';
     construtores.style.display = 'block';
+    im.style.height = '690px'
 }
